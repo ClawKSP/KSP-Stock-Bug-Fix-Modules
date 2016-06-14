fa8fc758771c82cdf3e71039e977e5a90d509439
ClawKSP <ClawKSP@gmail.com>
2016-06-13 20:18:30 -1000

    v00.02
    EditorCPUFix v00.02, includes fix for editor crash when deleting parts.

--- Source/EditorCPUFix.cs
+++ Source/EditorCPUFix.cs
@@ -18,6 +18,7 @@
 using UnityEngine;
 using KSP;
 using System.Collections;
+using KSP.UI.Screens;
 
 namespace ClawKSP
 {
@@ -26,7 +27,7 @@ namespace ClawKSP
     {
         public void Start ()
         {
-            Debug.Log("EditorCPUFix.Start(): v00.01");
+            Debug.Log("EditorCPUFix.Start(): v00.02");
             StartCoroutine(DisableDoodads());
         }
 
@@ -34,24 +35,24 @@ namespace ClawKSP
         IEnumerator DisableDoodads()
         {
             GameObject editorObject = null;
-            string crewName = "VABCrew";
+            //string crewName = "VABCrew";
 
             while (EditorDriver.fetch == null)
                 yield return null;
 
-            if (EditorDriver.editorFacility == EditorFacility.SPH)
-                crewName = "SPHCrew";
+            //if (EditorDriver.editorFacility == EditorFacility.SPH)
+            //    crewName = "SPHCrew";
 
-            if (GameSettings.SHOW_SPACE_CENTER_CREW)
-            {
-                while (editorObject == null)
-                {
-                    yield return null;
-                    editorObject = GameObject.Find(crewName);
-                }
-                editorObject = GameObject.Find(crewName);
-                if (editorObject) editorObject.SetActive(false);
-            }
+            //if (GameSettings.SHOW_SPACE_CENTER_CREW)
+            //{
+            //    while (editorObject == null)
+            //    {
+            //        yield return null;
+            //        editorObject = GameObject.Find(crewName);
+            //    }
+            //    editorObject = GameObject.Find(crewName);
+            //    if (editorObject) editorObject.SetActive(false);
+            //}
 
             editorObject = null;
             while (editorObject == null)
@@ -64,22 +65,71 @@ namespace ClawKSP
             if (EditorDriver.editorFacility == EditorFacility.SPH)
             {
                 editorObject = GameObject.Find("model_sph_interior_lights_v16");
-                if (editorObject != null) editorObject.SetActive(false);
-                editorObject = GameObject.Find("Lighting_Baked");
-                if (editorObject != null) editorObject.SetActive(false);
+                if (editorObject != null) editorObject.SetActive(GameSettings.SHOW_SPACE_CENTER_CREW);
+                //editorObject = GameObject.Find("Lighting_Baked");
+                //if (editorObject != null) editorObject.SetActive(GameSettings.SHOW_SPACE_CENTER_CREW);
             }
             else // VAB
             {
                 editorObject = GameObject.Find("model_vab_prop_truck_01");
-                if (editorObject != null) editorObject.SetActive(false);
+                if (editorObject != null) editorObject.SetActive(GameSettings.SHOW_SPACE_CENTER_CREW);
                 editorObject = GameObject.Find("model_vab_elevators");
-                if (editorObject != null) editorObject.SetActive(false);
-                editorObject = GameObject.Find("VAB_Interior_BakeLights");
-                if (editorObject != null) editorObject.SetActive(false);
+                if (editorObject != null) editorObject.SetActive(GameSettings.SHOW_SPACE_CENTER_CREW);
+                //editorObject = GameObject.Find("VAB_Interior_BakeLights");
+                //if (editorObject != null) editorObject.SetActive(GameSettings.SHOW_SPACE_CENTER_CREW);
                 editorObject = GameObject.Find("model_vab_interior_lights_flood_v16");
-                if (editorObject != null) editorObject.SetActive(false);
+                if (editorObject != null) editorObject.SetActive(GameSettings.SHOW_SPACE_CENTER_CREW);
             }
 
+        }
+    }
+
+    [KSPAddon(KSPAddon.Startup.EveryScene, false)]
+    public class EditorCrashFix : PartModule
+    {
+        private StageIcon stageIcon;
+
+        public void Start()
+        {
+            if (!(HighLogic.LoadedSceneIsEditor || HighLogic.LoadedSceneIsFlight)) return;
+
+            //Debug.Log("EditorCrashFix.Start(): v00.01");
+
+            if (part != null && part.stackIcon != null && part.stackIcon.StageIcon != null && part.stackIcon.StageIcon.gameObject != null)
+                stageIcon = part.stackIcon.StageIcon;
+        }
+
+        public void OnDestroy()
+        {
+            if (!(HighLogic.LoadedSceneIsEditor || HighLogic.LoadedSceneIsFlight)) return;
+
+            if (stageIcon != null && stageIcon.gameObject != null)
+                stageIcon.gameObject.SetActive(false);
+        }
+    }
+
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    public class EditorCrashHook : MonoBehaviour
+    {
+        private static bool modulesHooked = false;
+
+        public void Start()
+        {
+            if (modulesHooked) return;
+            modulesHooked = true;
+
+            Debug.LogWarning("EditorCrashHook.Start(): v00.01");
+
+            AvailablePart currentAP;
+
+            for (int indexParts = 0; indexParts < PartLoader.LoadedPartsList.Count; indexParts++)
+            {
+                currentAP = PartLoader.LoadedPartsList[indexParts];
+                if (currentAP != null && currentAP.partPrefab != null && currentAP.partPrefab.Modules != null && currentAP.partPrefab.Modules.Count > 0)
+                {
+                    currentAP.partPrefab.AddModule("EditorCrashFix");
+                }
+            }
         }
     }
 }
